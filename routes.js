cfddb2290ec7ef580791c018ae67dccd23eb19ee
krushndayshmookh <krushndayshmookh@gmail.com>
2019-03-14 01:05:56 +0530

    certificateController routes

--- routes.js
+++ routes.js
@@ -14,6 +14,16 @@ router.get('/team', (req, res) => {
 	res.render('team')
 })
 
+// Certificates -----
+
+const certificateController = require('./controllers/certificateController')
+
+router.get('/certificates/:event', certificateController.certificates_get)
+
+router.get('/certificates', (req, res) => res.redirect('/events'))
+
+router.get('/certificate/:id', certificateController.certificate_get)
+
 // Testing -----
 
 module.exports = router
